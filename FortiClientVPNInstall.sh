e36889acc182bb6e4be042e524f931cbcd03aa9c
Michael Permann <mike.permann@gmail.com>
2022-06-09 15:03:38 -0500

    Update FortiClientVPNInstall.sh
    Modifications to add using uninstall_helper method mentioned by seb.fisher on MacAdmins Slack channel.

--- FortiClientVPNInstall.sh
+++ FortiClientVPNInstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Name: FortiClientVPNRemoveInstall.sh
-# Version: 1.0.1
+# Version: 1.0.2
 # Created: 05-27-2022 by Michael Permann
-# Modified: 06-06-2022
+# Modified: 06-09-2022
 # Purpose: Detects whether an existing version of FortiClient VPN is installed. If it is, the 
 # settings are backed up, the uninstaller is executed with the prompting of the end user, the 
 # new version is installed and the settings are moved back in to place.
@@ -13,14 +13,10 @@ USER_ID=$(/usr/bin/id -u "$CURRENT_USER")
 LOGO="/Library/Application Support/HeartlandAEA11/Images/HeartlandLogo@512px.png"
 JAMF_HELPER="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
 JAMF_BINARY=$(which jamf)
-TITLE="Uninstall Application"
-DESCRIPTION="Another version of FortiClient VPN was detected and must be removed before proceeding. 
+TITLE="Notification"
+DESCRIPTION="Please ignore the two pop-up messages regarding \"FortiTray\" and \"System Extensions Blocked\". 
 
-1. Click the \"Uninstall\" button on the FortiClient Uninstaller dialog. 
-2. Enter your computer password and click the \"Install Helper\" button.
-3. Click the \"Done\" button to close FortiClient Uninstaller.
-
-Then click the \"OK\" button to dismiss this dialog."
+Click the \"OK\" button to dismiss this dialog."
 BUTTON1="OK"
 DEFAULT_BUTTON="1"
 TITLE1="Restart Needed"
@@ -28,20 +24,22 @@ DESCRIPTION1="Your computer needs restarted to complete the installation.
 
 Click the \"OK\" button to restart your computer. Your computer will restart automatically after 1 minute."
 VPN_APP="/Applications/FortiClient.app/Contents/MacOS/FortiClient"
+VPN_REMOVAL_APP="/Applications/FortiClientUninstaller.app/Contents/Library/LaunchServices/com.fortinet.forticlient.uninstall_helper"
 VPN_SETTINGS="/Library/Application Support/Fortinet/FortiClient/conf/vpn.plist"
 POLICY_TRIGGER_NAME="$4"
 POLICY_TRIGGER_NAME_SETTINGS="$5"
 
-if [ -f "$VPN_APP" ]
+if [ -f "$VPN_REMOVAL_APP" ]
 then
     echo "FortiClient software present"
     echo "Backing up vpn.plist"
     /bin/cp -p "$VPN_SETTINGS" "/tmp/vpn.plist"
-    echo "Launching FortiClientUninstaller.app"
-    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "/usr/bin/open" "/Applications/FortiClientUninstaller.app"
-    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$TITLE" -description "$DESCRIPTION" -icon "$LOGO" -button1 "$BUTTON1" -defaultButton "$DEFAULT_BUTTON"
+    echo "Running com.fortinet.forticlient.uninstall_helper removal app to remove software"  # Credit to seb.fisher on MacAdmins Slack #fortinet channel for finding this removal option
+    /Applications/FortiClientUninstaller.app/Contents/Library/LaunchServices/com.fortinet.forticlient.uninstall_helper
     echo "Updating inventory"
     "$JAMF_BINARY" recon
+    echo "Notify user about pop-up messages"
+    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$TITLE" -description "$DESCRIPTION" -icon "$LOGO" -button1 "$BUTTON1" -defaultButton "$DEFAULT_BUTTON" &
     echo "Installing FortiClient VPN software"
     "$JAMF_BINARY" policy -event "$POLICY_TRIGGER_NAME"
     echo "Updating inventory"
@@ -52,6 +50,8 @@ then
     /sbin/shutdown -r +1 &
 else
     echo "FortiClient software NOT present"
+    echo "Notify user about pop-up messages"
+    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$TITLE" -description "$DESCRIPTION" -icon "$LOGO" -button1 "$BUTTON1" -defaultButton "$DEFAULT_BUTTON" &
     echo "Installing FortiClient software"
     "$JAMF_BINARY" policy -event "$POLICY_TRIGGER_NAME"
     echo "Updating inventory"
